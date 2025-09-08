9e52901fc30eba2382fbd73a622163054877e97f
Matti <mattischlup@gmail.com>
2025-09-08 09:48:21 +0200

    added routing functionality with expo router

---
+++ notation/app/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useRouter } from 'expo-router';
+export default function HomeScreen() {
+	const router = useRouter();
+	return (
+		<View>
+			<Text>index.tsx</Text>
+			<Button title="Go To SecondPage.tsx" onPress={() => router.navigate('/secondPage')} />
+		</View>
+	);
+}
+
