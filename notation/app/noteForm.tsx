05b31bc6124a40c3595d9184ce76e32561630b3d
Utapu <96349282+Utapu@users.noreply.github.com>
2025-09-08 15:20:57 +0200

    feat: Create edit view
    * started on create
    
    * started on create
    
    * added create&edit view. untested. saves wrong. etc.
    
    * added view/edit page. added shared boolean to Note type. Overview now updatinig when new note is created
    
    * switched keyboard to numeric for radius
    
    * removed unecessary comments
    
    * removed weird ahh gitignorear file

---
+++ notation/app/noteForm.tsx
@@ -0,0 +1,118 @@
+import * as React from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, TextInput, Button } from "react-native-paper";
+import { setItem, getItem } from "../utils/AsyncStorage";
+import { useRouter, useLocalSearchParams } from "expo-router";
+import { Note } from "../models/note";
+
+export default function NoteForm() {
+  const { mode = "create", noteId } = useLocalSearchParams();
+  const router = useRouter();
+
+  const [title, setTitle] = React.useState("");
+  const [info, setInfo] = React.useState("");
+  const [location, setLocation] = React.useState<string | undefined>(undefined);
+  const [radius, setRadius] = React.useState<string | undefined>(undefined);
+
+  React.useEffect(() => {
+    if (mode === "edit" && noteId) {
+      loadNote();
+    }
+  }, [mode, noteId]);
+
+  const loadNote = async () => {
+    const note: Note | null = await getItem(`note-${noteId}`);
+    if (note) {
+      setTitle(note.title || "");
+      setInfo(note.info || "");
+      setLocation(note.location);
+      setRadius(note.radius);
+    }
+  };
+
+  const handleSave = async () => {
+    const note: Note = {
+      id: (noteId as string) || Date.now().toString(),
+      title,
+      info,
+      owner: "A",
+      location,
+      radius,
+    };
+
+    await setItem(`note-${note.id}`, note);
+    router.push("/"); 
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text variant="displayMedium">
+        {mode === "create" ? "Create" : "Edit"}
+      </Text>
+
+      <TextInput
+        label="Title"
+        value={title}
+        onChangeText={setTitle}
+        style={styles.input}
+      />
+
+      <TextInput
+        label="Info"
+        value={info}
+        onChangeText={setInfo}
+        style={styles.input}
+        multiline
+      />
+      <Text variant="labelMedium">
+        Add any important information to your Note
+      </Text>
+
+      <View style={styles.row}>
+        <Button
+          mode="outlined"
+          onPress={() => setLocation("Demo Location")}
+          style={styles.flex}
+        >
+          {mode === "create" ? "Add Location" : "Select Location"}
+        </Button>
+
+        <TextInput
+          label="Radius"
+          value={radius}
+	keyboardType="numeric"
+          onChangeText={setRadius}
+          style={[styles.flex, styles.input]}
+        />
+      </View>
+      <Text>Specify the radius for sending notifications</Text>
+
+      <View style={styles.row}>
+        <Button
+          mode="outlined"
+          onPress={() => router.push("/")}
+          style={styles.button}
+        >
+          Cancel
+        </Button>
+        <Button
+          mode="contained"
+          onPress={handleSave}
+          style={styles.button}
+          disabled={!title.trim()}
+        >
+          {mode === "create" ? "Create" : "Save"}
+        </Button>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: { padding: 20, gap: 10 },
+  input: { marginBottom: 10 },
+  row: { flexDirection: "row", alignItems: "center", gap: 10, marginTop: 10 },
+  flex: { flex: 1 },
+  button: { marginTop: 20, flex: 1 },
+});
+
---
+++ notation/models/note.ts
@@ -0,0 +1,9 @@
+export type Note = {
+	id: string;
+	title: string;
+	info: string;
+	owner: char;
+	location?: string;
+	radius?: string; 
+	shared?: bool;
+};
