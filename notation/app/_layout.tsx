f78818bd099cfe15a7a513b06d0b259f618c17ff
Miguel Lehmann <lehmann@puzzle.ch>
2025-09-08 10:45:04 +0200

    feat: Implement header

--- notation/app/_layout.tsx
+++ notation/app/_layout.tsx
@@ -1,10 +1,44 @@
 import { Slot } from "expo-router";
-import { View } from "react-native";
+import {Image, Platform, SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import React from "react";
 
 export default function Layout() {
   return (
-    <View style={{ flex: 1 }}>
-      <Slot />
-    </View>
+      <SafeAreaView style={styles.AndroidSafeArea} >
+      <View style={styles.header}>
+          <TouchableOpacity>
+              <Image
+                  source={require('../assets/Notation_Logo.png')}
+                  style={styles.logo}
+              />
+          </TouchableOpacity>
+
+
+          <TouchableOpacity>
+              <Text>Done</Text>
+          </TouchableOpacity>
+      </View>
+          <Slot />
+      </SafeAreaView>
   );
 }
+const styles = StyleSheet.create({
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingHorizontal: 16,
+        paddingVertical: 12,
+        backgroundColor: '#FFFFFF',
+    },
+    logo: {
+        width: 100,
+        height: 100,
+        borderRadius: 20,
+        resizeMode: 'contain',
+    },
+          AndroidSafeArea: {
+          flex: 1,
+          paddingTop: Platform.OS === 'android' ? 25 : 0
+      },
+});
--- notation/app/index.tsx
+++ notation/app/index.tsx
@@ -1,12 +1,13 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { useRouter } from 'expo-router';
+import {StyleSheet, Text, View, Button, SafeAreaView, Platform} from 'react-native';
+import {Slot, useRouter} from 'expo-router';
+import OverviewScreen from "./OvervewScreen";
+import Layout from "./_layout";
 export default function HomeScreen() {
 	const router = useRouter();
 	return (
-		<View>
-			<Text>index.tsx</Text>
-			<Button title="Go To SecondPage.tsx" onPress={() => router.navigate('/secondPage')} />
-		</View>
-	);
+        <View>
+            <Button title="Go To SecondPage.tsx" onPress={() => router.navigate('/secondPage')} />
+        </View>
+    );
 }
 
