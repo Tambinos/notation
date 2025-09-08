e8fbc59f39147ece604a5029964d8c17a9dd6548
Miguel Lehmann <lehmann@puzzle.ch>
2025-09-08 11:09:55 +0200

    style: Add paper styling to the header

--- notation/app/_layout.tsx
+++ notation/app/_layout.tsx
@@ -1,35 +1,43 @@
-import { Slot } from "expo-router";
-import {Image, Platform, SafeAreaView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {Slot} from "expo-router";
+import {Image, SafeAreaView, StyleSheet, TouchableOpacity, View} from "react-native";
 import React from "react";
+import { Appbar, Provider as PaperProvider } from 'react-native-paper';
+
 
 export default function Layout() {
-  return (
-      <SafeAreaView style={styles.AndroidSafeArea} >
-      <View style={styles.header}>
-          <TouchableOpacity>
-              <Image
-                  source={require('../assets/Notation_Logo.png')}
-                  style={styles.logo}
-              />
-          </TouchableOpacity>
-
-
-          <TouchableOpacity>
-              <Text>Done</Text>
-          </TouchableOpacity>
-      </View>
-          <Slot />
-      </SafeAreaView>
-  );
+    return (
+        <PaperProvider>
+            <SafeAreaView style={styles.safeArea}>
+                <Appbar.Header style={styles.header} elevated={true}>
+                    <TouchableOpacity>
+                        <Image
+                            source={require('../assets/Notation_Logo.png')}
+                            style={styles.logo}
+                        />
+                    </TouchableOpacity>
+                    <View style={{flex: 1}}/>
+                    <Appbar.Action
+                        icon="download"
+                        color="#5f6368"
+                        size={28}
+                    />
+                </Appbar.Header>
+                <Slot/>
+            </SafeAreaView>
+        </PaperProvider>
+    );
 }
+
+
 const styles = StyleSheet.create({
+    safeArea: {
+        flex: 1,
+    },
     header: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        paddingHorizontal: 16,
-        paddingVertical: 12,
         backgroundColor: '#FFFFFF',
+        justifyContent: 'space-between', // Ensures space between logo and action
+        alignItems: 'center',
+        height: 60, // A standard header height
     },
     logo: {
         width: 100,
@@ -37,8 +45,4 @@ const styles = StyleSheet.create({
         borderRadius: 20,
         resizeMode: 'contain',
     },
-          AndroidSafeArea: {
-          flex: 1,
-          paddingTop: Platform.OS === 'android' ? 25 : 0
-      },
 });
