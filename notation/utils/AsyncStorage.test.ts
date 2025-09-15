579ac5d20e891141067cce0ed8169196c6238c89
Miguel <141239860+Miguel7373@users.noreply.github.com>
2025-09-15 12:12:54 +0200

    test: add unit and snapshot tests
    * test: add unit and snapshot tests
    
    * test: Fix coordinates

--- notation/utils/AsyncStorage.test.ts
+++ notation/utils/AsyncStorage.test.ts
@@ -1,8 +1,10 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { getItem, getAllItems } from './AsyncStorage';
+import { getItem } from './AsyncStorage';
+import {Coordinates} from "../models/coordinates";
 
 jest.mock('@react-native-async-storage/async-storage', () => ({
     getItem: jest.fn(),
+    setItem: jest.fn(),
     removeItem: jest.fn(),
     getAllKeys: jest.fn(),
     multiGet: jest.fn(),
@@ -11,5 +13,40 @@ jest.mock('@react-native-async-storage/async-storage', () => ({
 describe('AsyncStorage helpers', () => {
     beforeEach(() => {
         jest.clearAllMocks();
+    });
+
+    describe('getItem', () => {
+        it('should get and parse an item from AsyncStorage when it exists', async () => {
+            const key = 'note';
+            const coordinates: Coordinates = { latitude: 1, longitude: 2 }
+            const value =  {
+                id: 1,
+                title: "Test",
+                info: "Test info",
+                owner: "test",
+                location: coordinates,
+                radius: "10",
+                shared: false
+            };
+            const stringifiedValue = JSON.stringify(value);
+
+            (AsyncStorage.getItem as jest.Mock).mockResolvedValue(stringifiedValue);
+
+            const result = await getItem(key);
+
+            expect(AsyncStorage.getItem).toHaveBeenCalledWith(key);
+            expect(result).toEqual(value);
+        });
+
+        it('should return null if the item does not exist in AsyncStorage', async () => {
+            const key = 'nonExistentKey';
+
+            (AsyncStorage.getItem as jest.Mock).mockResolvedValue(null);
+
+            const result = await getItem(key);
+
+            expect(AsyncStorage.getItem).toHaveBeenCalledWith(key);
+            expect(result).toBeNull();
+        });
     });
 });
